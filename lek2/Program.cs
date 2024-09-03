dd3f38785dde812331b7fcd240fbb64b7fa80f1b
Rasmus Eng <rasmus.eng@elev.ga.ntig.se>
2024-09-03 10:12:03 +0200

    first commit

---
+++ lek2/Program.cs
@@ -0,0 +1,19 @@
+﻿Skill attack = new()
+{
+Name = "Attack enemy",
+ManaCost = 10
+
+    
+};
+
+Skill heal = new(){
+
+    Name = "Heal self",
+    ManaCost = 20
+};
+
+Skill active = attack;
+
+Console.WriteLine(active.Name);
+Console.ReadLine();
+
